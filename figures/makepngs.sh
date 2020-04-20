e15187607f6a4121ea22d7fa9a14804e2f108c6a
Nathaniel J. Livesey <Nathaniel.J.Livesey@jpl.nasa.gov>
2020-04-19 21:27:24 -0700

    Raw import of Bill's new RHI section.

--- figures/makepngs.sh
+++ figures/makepngs.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 for file in *grid_*.pdf; do
     echo $file
-    convert -density 200 +antialias $file ${file%.*}-large.png
+    convert -density 300 +antialias $file ${file%.*}-large.png
     convert -resize 50% -antialias ${file%.*}-large.png ${file%.*}.png
     rm ${file%.*}-large.png
 done
