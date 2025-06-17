1bb75e25c3c599b767e024998b110568e9ea420d
Nathaniel Livesey <Nathaniel.J.Livesey@jpl.nasa.gov>
2025-06-17 13:55:45 -0700

    Removed a bunch of unused files

--- figures/makepdfs.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-for file in mls_h2o_v3-v4*.eps mls_rhi_v3-v4*.eps; do
-    echo $file
-    epstopdf $file
-done
--- figures/makepngs.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-for file in *grid_*.pdf; do
-    echo $file
-    convert -density 300 +antialias $file ${file%.*}-large.png
-    convert -resize 50% -antialias ${file%.*}-large.png ${file%.*}.png
-    rm ${file%.*}-large.png
-done
-    
